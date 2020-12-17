6aab757fedc025039aab147a986573ea70e243af
Richa Purba <richathorne@gmail.com>
2020-12-18 00:16:31 +0700

    Done with the hero design

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+    document.querySelector('.juice').src = anything;
+}
+function changeColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
