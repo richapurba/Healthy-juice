0a1dbc9ca7f38641704299bed305f8753b382119
Richa Purba <richathorne@gmail.com>
2020-12-18 23:47:06 +0700

    Responsive hero

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function changeColor(color) {
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+function hamburger() {
+    var hamburger = document.querySelector('.hamburger');
+    var navigator = document.querySelector('.navigator')
+    hamburger.classList.toggle('active');
+    navigator.classList.toggle('active');
+}
