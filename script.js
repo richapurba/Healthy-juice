f6b2f25597aeea0bd8a88c3c20d96c8557e7e957
Richa Purba <richathorne@gmail.com>
2020-12-19 21:45:48 +0700

    Responsive navbar

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ function changeColor(color) {
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
-function hamburger() {
-    var hamburger = document.querySelector('.hamburger');
-    var navigator = document.querySelector('.navigator')
-    hamburger.classList.toggle('active');
-    navigator.classList.toggle('active');
-}
+
+//Navigation bar
+const hamburger = document.getElementsByClassName('hamburger')[0];
+const navigator = document.getElementsByClassName('navigator')[0];
+
+hamburger.addEventListener('click', () => {
+    navigator.classList.toggle('active')
+})
